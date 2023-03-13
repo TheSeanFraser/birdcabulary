a9a823492d980ee8104fc5091da6e43af25ebc10
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2023-03-13 10:50:49 -0400

    Continued quiz formatting

---
+++ js/playSound.js
@@ -0,0 +1,5 @@
+function playSound(){
+	var audio = new Audio('https://theseanfraser.github.io/birdcabulary/sounds/BWWA/songs/XC558029%20-%20Blue-winged%20Warbler%20-%20Vermivora%20cyanoptera.mp3');
+	audio.play();
+	console.log("playing sound");
+}
